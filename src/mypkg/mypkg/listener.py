50fc773c5c83bded3c9de0f025656b85106a9426
Yamashita <Yamashitaaoi1230@icloud.com>
2024-01-03 01:02:57 +0900

    first commit

---
+++ src/mypkg/mypkg/listener.py
@@ -0,0 +1,11 @@
+import rclpy
+from rclpy.node import Node
+from person_msgs.msg import Person
+
+def cb(msg):
+    node.get_logger().info("Listen:%s" % msg)
+
+rclpy.init()
+node = Node("listener")
+sub = node.create_subscription(Person,"person",cb,10)
+rclpy.spin(node)
